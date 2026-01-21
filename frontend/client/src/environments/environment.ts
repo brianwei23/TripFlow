507e621d24acb6983c7ebbed3c2678cf9028893d
brianwei23 <brianwei03@gmail.com>
2026-01-20 17:37:26 -0800

    Initial setup

---
+++ frontend/client/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD2HKHMnQOx5MwYg-IC-MCBm27AU0TOTKo",
+    authDomain: "tripflow-app-d3e2c.firebaseapp.com",
+    projectId: "tripflow-app-d3e2c",
+    storageBucket: "tripflow-app-d3e2c.firebasestorage.app",
+    messagingSenderId: "500567246920",
+    appId: "1:500567246920:web:a22f7d77b0fed59b81307d",
+    measurementId: "G-LMFQSLBNVF"
+  }
+};
---
+++ frontend/client/src/main.ts
@@ -0,0 +1,22 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router'; 
+import { environment } from './environments/environment';
+import 'zone.js';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideHttpClient(),
+    importProvidersFrom(
+      RouterModule.forRoot([]) 
+    )
+  ]
+});
